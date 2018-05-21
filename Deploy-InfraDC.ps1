0a9a52d5898d0f4f3422aab43d0b33410216470c
Ryan Bernardino <r.bernardino17@gmail.com>
2018-05-21 11:59:27 +0800

    Moves network resources into separate repo

--- Deploy-InfraDC.ps1
+++ Deploy-InfraDC.ps1
@@ -58,27 +58,6 @@ break
 
 #endregion
 
-#region Deploy the lab environment network resource
-  $location = 'Southeast Asia'
-  $resourceGroupName = 'dscLabNetwork'
-  $resourceDeploymentName = 'dscLabNetwork-deployment'
-  $templateFile = '.\AzRmTemplates\dsclab.network.json'
-  $templateParameterFile = '.\AzRmTemplates\dsclab.network.parameters.json'
-
-  New-AzureRmJsonTemplateDeployment `
-    -ResourceGroupName $resourceGroupName `
-    -DeploymentName $resourceDeploymentName `
-    -Location $location `
-    -Path $templateFile `
-    -TemplateParameterFile $templateParameterFile `
-    -Verbose -Force
-
-  # Cleanup if needed
-  Remove-AzureRmResourceGroup `
-    -Name $resourceGroupName `
-    -Force -Verbose
-#endregion
-
 #region Deploy the VM
 {
   $location = 'Southeast Asia'
